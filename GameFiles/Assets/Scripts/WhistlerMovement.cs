103f9f781a72297b0417930afd2fa3c0d373d27e
maxthibeau <thibeaumax@gmail.com>
2018-04-03 19:00:38 -0600

    added ai functionality
    this includes: the whistler slowly rotates towards the player, when the whistler and player get close damage is applied to the player, and the whistler has three established states. All the states do right now is change the speed, but they're the framework for the whistler ai.

--- GameFiles/Assets/Scripts/WhistlerMovement.cs
+++ GameFiles/Assets/Scripts/WhistlerMovement.cs
@@ -3,44 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class WhistlerMovement : MonoBehaviour {
+public class WhistlerMovement : MonoBehaviour
+{
 
-    Transform player;
-    Transform whistler;
-    NavMeshAgent agent;
-    NavMeshPath path;
+    public Transform player;
+    public Transform whistler;
+    public NavMeshAgent agent;
+    public NavMeshPath path;
+    public triggerDamage triggerDamage;
+    public BoxCollider whistlerCollider;
+
+    public int fov = 90;
+    public float aggro_speed = 2.5F;
+    public float wander_speed = 1.5F;
+    public float search_speed = 2F;
+    public float rotate_speed = .001F;
+    public float damage_range = 4F;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        whistler = GameObject.FindGameObjectWithTag("Whistler").transform;
-        agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(player.position);  
+        agent.SetDestination(player.position);
         agent.autoBraking = false;
 
         path = new NavMeshPath();
         bool hasFoundPath = agent.CalculatePath(player.position, path);
-
-        if (path.status == NavMeshPathStatus.PathComplete)
-        {
-            print("The agent can reach the destionation");
-        }
-        else if (path.status == NavMeshPathStatus.PathPartial)
-        {
-            print("The agent can only get close to the destination");
-        }
-        else if (path.status == NavMeshPathStatus.PathInvalid)
-        {
-            print("The agent cannot reach the destination");
-            print("hasFoundPath will be false");
-        }
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
         agent.SetDestination(player.position);
         path = new NavMeshPath();
+        //whistler.LookAt(player);
+        rotate();
         agent.CalculatePath(player.position, path);
+        if (LineOfSight(whistler, player))
+        {
+            print("found player");
+            aggro();
+        }
+        else
+        {
+            wander();
+        }
+        triggerDamage.OnTriggerStay(whistlerCollider);
+        if(Vector3.Distance(whistler.position, player.position) <= damage_range)
+        {
+            print("attacking player");
+            StartCoroutine(triggerDamage.applyPlayerDamage(player.gameObject));
+        }
+    }
+
+    bool LineOfSight(Transform transform, Transform target)
+    {
+        if (Vector3.Angle(transform.position - target.position, transform.forward) <= fov &&
+        Physics.Linecast(transform.position, target.position))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void rotate()
+    {
+
+
+        Quaternion wantedRotation = Quaternion.LookRotation(player.position - whistler.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, wantedRotation, Time.time * rotate_speed);
+
+    }
+
+    void aggro()
+    {
+        agent.speed = aggro_speed;
+    }
+
+    void wander()
+    {
+
+        agent.speed = wander_speed;
+    }
+
+    void search()
+    {
+        agent.speed = search_speed;
     }
 }
