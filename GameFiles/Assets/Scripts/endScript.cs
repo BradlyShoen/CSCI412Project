ee14a441a026649350473f64e1211f1b78ce06a0
Brad Shoen <bradlyshoen@gmail.com>
2018-04-27 14:29:12 -0600

    Added Ending Scene Animation and Final Message

---
+++ GameFiles/Assets/Scripts/endScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endScript : MonoBehaviour {
+	public GameObject endMessage;
+	
+	void Update () {
+		if(!GetComponent<Animation>().isPlaying){
+			endMessage.SetActive(true);
+		}
+	}
+}
