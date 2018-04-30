08ff688bac1a2029d8f4c2f9d32cd4f4be787a00
Brad Shoen <bradlyshoen@gmail.com>
2018-04-29 19:15:49 -0600

    Added Items Throughout The Map and Made Build 1.0 for Final Presentation

--- GameFiles/Assets/Scripts/gameManagerLogic.cs
+++ GameFiles/Assets/Scripts/gameManagerLogic.cs
@@ -10,15 +10,27 @@ public class gameManagerLogic : MonoBehaviour {
 	public GameObject eightItems;
 	public GameObject rootObject;
 	
+	private bool devCommand = false;
+	
 	void Start () {
 		//Randomly spawn the whistler somewhere on the map
 		whistlerObject.transform.position = new Vector3(Random.Range(-500f, 500f), transform.position.y, Random.Range(-500f, 500f));
 	}
 
 	void Update(){
-		if (eightItems.transform.childCount == 0) {
+		if (eightItems.transform.childCount == 0 || devCommand) {
 			SceneManager.LoadScene("EndingScene", LoadSceneMode.Additive);
 			Destroy (rootObject);
+		}
+		
+		if(playerObject != null && playerObject.transform.position.y < -200){
+			playerObject.GetComponent<playerLogic>().TakeDamage(100);
+		}
+		
+		if(Input.GetKey("o")){
+			if(Input.GetKey("p")){
+				devCommand = true;
+			}
 		}
 	}
 }
